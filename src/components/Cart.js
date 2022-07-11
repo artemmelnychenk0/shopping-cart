dbec26fc8ab14e1cd95d93139e52fe3a243d612e
artem <amelnychenko06@gmail.com>
2022-07-10 20:33:56 -0400

    Updated cart so it shows items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,29 +1,58 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import "../styles/route.css"
 
-const Cart = (props) => {
+
+
+const Cart = ({ cart }) => {
+
+    const selected = cart
+    console.log(selected)
 
     useEffect(() => {
         fetchItems();
+
     }, []);
 
-    const [items, setItems] = useState([]);
+    const [items, setItem] = useState([]);
+
+
 
     const fetchItems = async () => {
-        const data = await fetch('https://fakestoreapi.com/products/1');
+        const data = await fetch(`https://fakestoreapi.com/products`);
         const products = await data.json()
-        console.log(products)
-        setItems(products);
-        console.log(items)
+
+
+        for (let i = 0; i < selected.length; i++) {
+
+            setItem(arr => [...arr, products[selected[i]]])
+            console.log(items)
+        }
+
+
+
     }
 
     return (
         <div className="sidecart">
             <h1>Cart</h1>
             <div className="display">
-                <img src={items.image} alt={items.title}></img>
-                <div>{items.title}</div>
-                <div>{items.price}</div>
+                {items.map(item => (
+
+                    <div key={item.id} className="item">
+
+                        <img src={item.image} alt={item.title}></img>
+
+                        <div>{item.title}</div>
+
+                        <div>${Number(item.price).toFixed(0)}.00</div>
+
+
+
+
+                    </div>
+
+                ))}
             </div>
         </div>
     )
