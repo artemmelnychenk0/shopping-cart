e900a828b8f3753cabf8273c7be383d6d729ffa9
artem <amelnychenko06@gmail.com>
2022-07-10 20:31:38 -0400

    added some functionality for Cart element

--- src/components/routes/RouteSwitch.js
+++ src/components/routes/RouteSwitch.js
@@ -4,16 +4,28 @@ import Navbar from "../Navbar";
 import Shop from "../Shop";
 import Contact from "../Contact";
 import Cart from "../Cart";
+import Product from "../Product";
+import { useState } from "react";
 
 const RouteSwitch = () => {
+
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (current) => {
+        const num = Number(current) - 1;
+        setCart(cart.concat(num))
+        console.log(cart)
+    }
     return (
         <BrowserRouter>
             <Navbar />
+
             <Routes>
                 <Route path='/' element={<Homepage />} />
                 <Route path='/shop' element={<Shop />} />
+                <Route path='shop/shop/:id' element={<Product addToCart={addToCart} />} />
                 <Route path='/contact' element={<Contact />} />
-                <Route path='/cart' element={<Cart />} />
+                <Route path='/cart' element={<Cart cart={cart} />} />
             </Routes>
         </BrowserRouter>
     )
