50306763b1ed41c895879f9437227e2fbd822947
artem <amelnychenko06@gmail.com>
2022-07-12 15:58:14 -0400

    Added a contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { AiOutlineFacebook, AiOutlineInstagram } from 'react-icons/ai'
+import { FiTwitter } from 'react-icons/fi'
+
 const Contact = () => {
     return (
         <div>
-            <h1>Contact</h1>
+            <Navbar fixed="bottom" bg="dark" variant="dark" >
+                <Container>
+                    <Navbar.Brand bsPrefix="bottom">Contact Us</Navbar.Brand>
+                    <Nav className="justify-content-center">
+                        <Link to="#" className="nav-link"><AiOutlineFacebook size='2em' /></Link>
+                        <Link to="#" className="nav-link"><AiOutlineInstagram size='2em' /></Link>
+                        <Link to="#" className="nav-link"><FiTwitter size='2em' /></Link>
+                    </Nav>
+
+                </Container>
+            </Navbar>
         </div>
     )
 }
