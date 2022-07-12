662fd0325661f2c79e462b39a3c2fec0c36f3926
artem <amelnychenko06@gmail.com>
2022-07-12 15:57:47 -0400

    Added styling and finished all the features

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,52 +1,40 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
-import "../styles/route.css"
-import { Button } from "react-bootstrap/Button"
-
+import { MdOutlineDeleteForever } from 'react-icons/md'
 
 const Cart = ({ cart, deleteForever }) => {
 
     const selected = cart
-    // console.log(selected)
+
     const [items, setItem] = useState([]);
     const [total, setTotal] = useState();
     const [quantity, setQuantity] = useState(0);
 
-
     useEffect(() => {
         fetchItems();
-
     }, []);
 
     useEffect(() => {
         addQuantity();
     }, [quantity])
 
-
     useEffect(() => {
         calculateTotal();
     }, [items])
 
-
     const fetchItems = async () => {
         const data = await fetch(`https://fakestoreapi.com/products`);
         const products = await data.json()
-        console.log(products)
-
         for (let i = 0; i < selected.length; i++) {
-
             setItem(arr => [...arr, products[selected[i]]])
-            // console.log(items)
         }
         setQuantity(1)
-
-        console.log(items)
-
     }
 
     const addQuantity = () => {
         setItem(items.map(arr => ({ ...arr, qty: 1 })))
     }
+
     const calculateTotal = () => {
         const total = items.reduce((accumulator, object) => {
             return accumulator + (object.price * object.qty);
@@ -54,10 +42,7 @@ const Cart = ({ cart, deleteForever }) => {
         setTotal(total.toFixed(0));
     }
 
-
-
     const increase = (arg, arg2) => {
-
         setItem(items.map(item =>
             item.id === arg
                 ? { ...item, qty: arg2 + 1 }
@@ -65,6 +50,7 @@ const Cart = ({ cart, deleteForever }) => {
         calculateTotal();
 
     }
+
     const decrease = (arg, arg2) => {
         setItem(items.map(item =>
             item.id === arg
@@ -78,37 +64,28 @@ const Cart = ({ cart, deleteForever }) => {
         setItem(items.filter(a => a.id !== arg))
     }
 
-
-
     return (
         <div className="sidecart">
-            <h1>Cart</h1>
-            <div className="display">
+            <div className="display-cart">
                 {items.map(item => (
 
-                    <div key={item.id} className="item">
-
+                    <div key={item.id} className="item-cart">
                         <img src={item.image} alt={item.title}></img>
-
                         <div>{item.title}</div>
-
                         <div>${Number(item.price).toFixed(0)}.00</div>
-
-
-
-                        <button className="btn btn-dark btn-sm" onClick={() => decrease(item.id, item.qty)} >-</button>
-                        <div>Qunatity: {item.qty}</div>
+                        <div>Qunatity: </div>
+                        <button onClick={() => decrease(item.id, item.qty)} >-</button>
+                        <div>{item.qty}</div>
                         <button onClick={() => increase(item.id, item.qty)} value={item.id}>+</button>
-                        <button onClick={() => { deleteProduct(item.id); deleteForever(item.id); }}>Delete</button>
-
-
-
-
+                        <button className='delete' onClick={() => { deleteProduct(item.id); deleteForever(item.id); }}><MdOutlineDeleteForever size='2em' /></button>
                     </div>
 
                 ))}
             </div>
-            <div>Total:${total}.00</div>
+            <div className="cart-total">Total:${total}.00</div>
+            <div className="container-btn">
+                <button className="checkout">Proceed to Checkout</button>
+            </div>
         </div>
     )
 }
