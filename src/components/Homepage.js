3ff2b14d43ac103bb250d976faa1947debcac632
artem <amelnychenko06@gmail.com>
2022-07-12 15:58:34 -0400

    Styled homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+import main from '../assets/main.jpg'
+
 const Homepage = () => {
     return (
         <div>
-            <h1>Home page</h1>
+            <img src={main} alt='main' className='main'></img>
+            <div className="brand">StarLight</div>
+            <div className="centered">We don't do fashion, we are fashion</div>
+            <Link to="/shop">
+                <button className='shop-now'>Shop now</button>
+            </Link>
         </div>
     )
 }
